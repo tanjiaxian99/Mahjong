22e33c7493d81647223e7560dcb211a6255cda88
Jia Xian <tan.jia.xian.2016@vjc.sg>
2020-05-01 01:10:07 +0800

    Added TestCube and ViewportHandler code from stackoverflow

---
+++ Assets/Scripts/CameraWork.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWork : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    #region Private Methods
+
+    
+
+    #endregion
+}
