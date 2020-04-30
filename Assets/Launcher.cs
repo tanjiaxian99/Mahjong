b3492a45b298476dfd154da48ee8c1959885f7b0
Jia <tan.jia.xian.2016@vjc.sg>
2020-04-30 12:11:19 +0800

    Launcher script skeleton

--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -5,6 +5,7 @@ using Photon.Pun;
 
 public class Launcher : MonoBehaviour {
     #region Private Fields
+
     /// <summary>
     /// This client's version number. Users are separated from each other by gameVersion.
     /// </summary>
@@ -18,4 +19,28 @@ public class Launcher : MonoBehaviour {
         // All clients in the same room will automatically load the same level as Master Client
         PhotonNetwork.AutomaticallySyncScene = true;
     }
+
+
+    void Start() {
+        Connect();
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    /// <summary>
+    /// Start the connection process
+    /// If connected, join a random room. Else, connect to server
+    /// </summary>
+    public void Connect() {
+        if (PhotonNetwork.IsConnected) {
+            PhotonNetwork.JoinRandomRoom();
+        } else {
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+        }
+    }
+
+    #endregion
 }
