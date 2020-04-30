15522c4fe2c9085ef1220428a177863e9113364c
Jia Xian <tan.jia.xian.2016@vjc.sg>
2020-04-30 20:42:37 +0800

    Added PlayerManager.cs

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Pun;
+
+public class PlayerManager : MonoBehaviourPunCallbacks {
+    #region Private Fields
+
+    private int score;
+
+    #endregion
+}
