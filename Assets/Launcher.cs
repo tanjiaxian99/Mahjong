15644279ec7df3c13e229b1a12f252721e1ead07
Jia <tan.jia.xian.2016@vjc.sg>
2020-04-30 12:02:39 +0800

    Added Photon assets

--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Launcher : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class Launcher : MonoBehaviour {
+    #region Private Fields
+    /// <summary>
+    /// This client's version number. Users are separated from each other by gameVersion.
+    /// </summary>
+    string gameVersion = "1";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    #endregion
+
+    #region MonoBehaviour CallBacks
+
+    void Awake() {
+        // All clients in the same room will automatically load the same level as Master Client
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 }
