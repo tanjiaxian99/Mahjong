158bc1ec6364b61194380383b039fe7d9131c1c0
Jia Xian <tan.jia.xian.2016@vjc.sg>
2020-05-05 13:33:09 +0800

    Adjusted pivot for remaining 44 tiles

---
+++ Assets/Scripts/InstantiateTile.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateTile : MonoBehaviour {
+    public GameObject gm;
+
+    // Start is called before the first frame update
+    void Start() {
+        Instantiate(gm, new Vector3(0f, 2f, 0f), Quaternion.Euler(270f, 180f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
