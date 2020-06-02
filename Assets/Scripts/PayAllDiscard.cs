43351dad2f7432da87b98d5661ad720900520980
Jia Xian <tan.jia.xian.2016@vjc.sg>
2020-06-02 13:14:00 +0800

    Added and finished FullFlush.

--- Assets/Scripts/PayAllDiscard.cs
+++ Assets/Scripts/PayAllDiscard.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.XR;
 
@@ -11,6 +12,7 @@ public class PayAllDiscard {
     private Dictionary<string, int> handsToCheck;
     private Dictionary<Tile, PlayerManager.Wind> bonusTileToWindDict;
     private Dictionary<PlayerManager.Wind, List<Tile>> windToTileDict;
+    private Dictionary<Tile.Suit?, List<Tile>> fullFlushDict;
 
     private Tile dragonOne;
     private Tile dragonTwo;
@@ -33,37 +35,9 @@ public class PayAllDiscard {
         this.bonusTileToWindDict = new Dictionary<Tile, PlayerManager.Wind>();
         this.windToTileDict = new Dictionary<PlayerManager.Wind, List<Tile>>();
 
-        this.windToTileDict = new Dictionary<PlayerManager.Wind, List<Tile>>();
-        this.windToTileDict.Add(PlayerManager.Wind.EAST, new List<Tile>() { 
-            new Tile(Tile.Suit.Season, Tile.Rank.One), 
-            new Tile(Tile.Suit.Flower, Tile.Rank.One),
-            new Tile(Tile.Suit.Wind, Tile.Rank.One) });
-        this.windToTileDict.Add(PlayerManager.Wind.SOUTH, new List<Tile>() {
-            new Tile(Tile.Suit.Season, Tile.Rank.Two), 
-            new Tile(Tile.Suit.Flower, Tile.Rank.Two),
-            new Tile(Tile.Suit.Wind, Tile.Rank.Two)});
-        this.windToTileDict.Add(PlayerManager.Wind.WEST, new List<Tile>() { 
-            new Tile(Tile.Suit.Season, Tile.Rank.Three), 
-            new Tile(Tile.Suit.Flower, Tile.Rank.Three),
-            new Tile(Tile.Suit.Wind, Tile.Rank.Three)});
-        this.windToTileDict.Add(PlayerManager.Wind.NORTH, new List<Tile>() { 
-            new Tile(Tile.Suit.Season, Tile.Rank.Four),
-            new Tile(Tile.Suit.Flower, Tile.Rank.Four),
-            new Tile(Tile.Suit.Wind, Tile.Rank.Four)});
-
-        this.dragonOne = new Tile(Tile.Suit.Dragon, Tile.Rank.One);
-        this.dragonTwo = new Tile(Tile.Suit.Dragon, Tile.Rank.Two);
-        this.dragonThree = new Tile(Tile.Suit.Dragon, Tile.Rank.Three);
-
-        this.windOne = new Tile(Tile.Suit.Wind, Tile.Rank.One);
-        this.windTwo = new Tile(Tile.Suit.Wind, Tile.Rank.Two);
-        this.windThree = new Tile(Tile.Suit.Wind, Tile.Rank.Three);
-        this.windFour = new Tile(Tile.Suit.Wind, Tile.Rank.Four);
-
-        this.animalOne = new Tile(Tile.Suit.Animal, Tile.Rank.One);
-        this.animalTwo = new Tile(Tile.Suit.Animal, Tile.Rank.Two);
-        this.animalThree = new Tile(Tile.Suit.Animal, Tile.Rank.Three);
-        this.animalFour = new Tile(Tile.Suit.Animal, Tile.Rank.Four);
+        this.InitializeWindToTileDict();
+        this.InitializeFullFlushDict();
+        this.InitializeTiles();
     }
 
     /// <summary>
@@ -164,7 +138,7 @@ public class PayAllDiscard {
             }
 
             if (tile.suit == Tile.Suit.Dragon) {
-                totalFan += handsToCheck["Dragon"];
+                highRiskDict[tile] += handsToCheck["Dragon"];
             }
 
             if (tile.suit == Tile.Suit.Animal) {
@@ -204,10 +178,170 @@ public class PayAllDiscard {
         }
 
         // If fan limit is 5, there are high risk discards only when there are at least 3 fan.
-        if (totalFan >= handsToCheck["Fan Limit"] - 2) {
+        if (totalFan == handsToCheck["Fan Limit"] - 2) {
+            if (playerWind == prevailingWind && highRiskDict[windToTileDict[playerWind][2]] == 0) {
+                highRiskTiles.Add(windToTileDict[playerWind][2]);
+                return highRiskTiles;
+            }
 
+        } else if (totalFan >= handsToCheck["Fan Limit"] - 1) {
+            foreach (Tile tile in highRiskDict.Keys) {
+                if (highRiskDict[tile] == 0) {
+                    highRiskTiles.Add(tile);
+                }
+            }
         }
 
         return highRiskTiles;
     }
+
+
+    /// <summary>
+    /// Determine the high risk discards in Full Flush Scenario
+    /// </summary>
+    public List<Tile> FullFlush(List<Tile> openTiles) {
+        List<Tile> highRiskTiles = new List<Tile>();
+        Tile.Suit? referenceSuit = openTiles[openTiles.Count - 1].suit;
+        int referenceSuitCount = 0;
+
+        if (referenceSuit == Tile.Suit.Season || referenceSuit == Tile.Suit.Flower || referenceSuit == Tile.Suit.Animal) {
+            return highRiskTiles;
+        }
+
+        foreach (Tile tile in openTiles) {
+            if (tile.suit != referenceSuit) {
+                if (tile.suit != Tile.Suit.Season && tile.suit != Tile.Suit.Flower && tile.suit != Tile.Suit.Animal) {
+                    return highRiskTiles;
+                }
+
+                if (tile.suit == Tile.Suit.Wind && referenceSuit == Tile.Suit.Dragon) {
+                    referenceSuitCount++;
+                }
+
+                if (tile.suit == Tile.Suit.Dragon && referenceSuit == Tile.Suit.Wind) {
+                    referenceSuitCount++;
+                }
+
+            } else {
+                referenceSuitCount++;
+            }
+        }
+
+        if (referenceSuitCount < 9) {
+            return highRiskTiles;
+        }
+
+        return fullFlushDict[referenceSuit];
+
+    }
+
+    #region Initializer Functions
+
+    /// <summary>
+    /// Initialize the Wind to Tile Dictionary
+    /// </summary>
+    private void InitializeWindToTileDict() {
+        this.windToTileDict = new Dictionary<PlayerManager.Wind, List<Tile>>();
+        this.windToTileDict.Add(PlayerManager.Wind.EAST, new List<Tile>() {
+            new Tile(Tile.Suit.Season, Tile.Rank.One),
+            new Tile(Tile.Suit.Flower, Tile.Rank.One),
+            new Tile(Tile.Suit.Wind, Tile.Rank.One) });
+        this.windToTileDict.Add(PlayerManager.Wind.SOUTH, new List<Tile>() {
+            new Tile(Tile.Suit.Season, Tile.Rank.Two),
+            new Tile(Tile.Suit.Flower, Tile.Rank.Two),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Two)});
+        this.windToTileDict.Add(PlayerManager.Wind.WEST, new List<Tile>() {
+            new Tile(Tile.Suit.Season, Tile.Rank.Three),
+            new Tile(Tile.Suit.Flower, Tile.Rank.Three),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Three)});
+        this.windToTileDict.Add(PlayerManager.Wind.NORTH, new List<Tile>() {
+            new Tile(Tile.Suit.Season, Tile.Rank.Four),
+            new Tile(Tile.Suit.Flower, Tile.Rank.Four),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Four)});
+    }
+
+
+    /// <summary>
+    /// Initializes the Full Flush Dictionary
+    /// </summary>
+    private void InitializeFullFlushDict() {
+        this.fullFlushDict = new Dictionary<Tile.Suit?, List<Tile>>();
+        fullFlushDict.Add(Tile.Suit.Character, new List<Tile>() {
+            new Tile(Tile.Suit.Character, Tile.Rank.One),
+            new Tile(Tile.Suit.Character, Tile.Rank.Two),
+            new Tile(Tile.Suit.Character, Tile.Rank.Three),
+            new Tile(Tile.Suit.Character, Tile.Rank.Four),
+            new Tile(Tile.Suit.Character, Tile.Rank.Five),
+            new Tile(Tile.Suit.Character, Tile.Rank.Six),
+            new Tile(Tile.Suit.Character, Tile.Rank.Seven),
+            new Tile(Tile.Suit.Character, Tile.Rank.Eight),
+            new Tile(Tile.Suit.Character, Tile.Rank.Nine)
+        });
+
+        fullFlushDict.Add(Tile.Suit.Dot, new List<Tile>() {
+            new Tile(Tile.Suit.Dot, Tile.Rank.One),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Two),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Three),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Four),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Five),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Six),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Seven),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Eight),
+            new Tile(Tile.Suit.Dot, Tile.Rank.Nine)
+        });
+
+        fullFlushDict.Add(Tile.Suit.Bamboo, new List<Tile>() {
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.One),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Two),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Three),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Four),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Five),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Six),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Seven),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Eight),
+            new Tile(Tile.Suit.Bamboo, Tile.Rank.Nine)
+        });
+
+        fullFlushDict.Add(Tile.Suit.Wind, new List<Tile>() {
+            new Tile(Tile.Suit.Wind, Tile.Rank.One),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Two),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Three),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Four),
+            new Tile(Tile.Suit.Dragon, Tile.Rank.One),
+            new Tile(Tile.Suit.Dragon, Tile.Rank.Two),
+            new Tile(Tile.Suit.Dragon, Tile.Rank.Three),
+        });
+
+        fullFlushDict.Add(Tile.Suit.Dragon, new List<Tile>() {
+            new Tile(Tile.Suit.Wind, Tile.Rank.One),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Two),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Three),
+            new Tile(Tile.Suit.Wind, Tile.Rank.Four),
+            new Tile(Tile.Suit.Dragon, Tile.Rank.One),
+            new Tile(Tile.Suit.Dragon, Tile.Rank.Two),
+            new Tile(Tile.Suit.Dragon, Tile.Rank.Three),
+        });
+    }
+
+
+    /// <summary>
+    /// Initialize tiles
+    /// </summary>
+    private void InitializeTiles() {
+        this.dragonOne = new Tile(Tile.Suit.Dragon, Tile.Rank.One);
+        this.dragonTwo = new Tile(Tile.Suit.Dragon, Tile.Rank.Two);
+        this.dragonThree = new Tile(Tile.Suit.Dragon, Tile.Rank.Three);
+
+        this.windOne = new Tile(Tile.Suit.Wind, Tile.Rank.One);
+        this.windTwo = new Tile(Tile.Suit.Wind, Tile.Rank.Two);
+        this.windThree = new Tile(Tile.Suit.Wind, Tile.Rank.Three);
+        this.windFour = new Tile(Tile.Suit.Wind, Tile.Rank.Four);
+
+        this.animalOne = new Tile(Tile.Suit.Animal, Tile.Rank.One);
+        this.animalTwo = new Tile(Tile.Suit.Animal, Tile.Rank.Two);
+        this.animalThree = new Tile(Tile.Suit.Animal, Tile.Rank.Three);
+        this.animalFour = new Tile(Tile.Suit.Animal, Tile.Rank.Four);
+    }
+    #endregion
+
 }
