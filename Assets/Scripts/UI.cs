82b09960ff7812b4cba172a3bb0ae85a49e88bb4
Jia Xian <tan.jia.xian.2016@vjc.sg>
2020-06-24 23:51:34 +0800

    Added a General Panel which will be used for most UI displays, excluding Chow, Pong and Kong.

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+    // TODO: Sacred Discard, Missed Discard, Instant Payout, OnWinOk, Winning, EndGame, Number of tiles left, Start new round, Seat Wind, Prevailing Wind, Nicknames, Points
+    
+}
